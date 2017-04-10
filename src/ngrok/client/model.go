60a8d84f68569a6bc320f8e283aedbafbefb3da2
Stuart Allen <stuart@valid8.com>
2017-04-10 13:34:00 -0700

    correct server port

--- src/ngrok/client/model.go
+++ src/ngrok/client/model.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	defaultServerAddr   = "tools.valid8.com:443"
+	defaultServerAddr   = "tools.valid8.com:4443"
 	defaultInspectAddr  = "127.0.0.1:4040"
 	pingInterval        = 20 * time.Second
 	maxPongLatency      = 15 * time.Second
